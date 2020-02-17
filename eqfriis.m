bf4a12112be85c5d6252b3e95e29e056da5f715b
Alisson Boeing <alisson.b11@aluno.ifsc.edu.br>
2020-02-17 15:13:52 -0300

    Adicao da Eq de 2 raios

--- eqfriis.m
+++ eqfriis.m
@@ -6,6 +6,8 @@ f = 900e6;
 c = 3e8;
 lamb = c/f;
 
+ht = 50;
+hr = 3;
 
 
 Gt = 1;
@@ -18,7 +20,12 @@ L = 1;
 
 Pr = (Pt*Gt*Gr*(lamb^2))./(((4*3.14)^2).*(d.^2)*L);
 
+Pr2 = (Pt*Gt*Gr*(ht^2)*(hr^2))./(d.^4);
 
 Prdbm =  10*log10(Pr/0.01);
 
+Pr2dbm = 10*log10(Pr2/0.01);
+
 semilogx(d,Prdbm);
+hold on
+semilogx(d, Pr2dbm);
